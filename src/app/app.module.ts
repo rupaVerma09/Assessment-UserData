3ebcae3787f231880e423b93b455abbed82d3789
rupaVerma09 <66214117+rupaVerma09@users.noreply.github.com>
2021-05-29 20:52:51 +0530

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormComponentComponent } from './form-component/form-component.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponentComponent
+    
   ],
   imports: [
     BrowserModule,
